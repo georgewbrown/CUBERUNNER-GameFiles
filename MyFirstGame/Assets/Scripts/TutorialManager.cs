7cc450411294f8092e690a7d1858d439495a3b7a
georgewbrown <396gbrown@gmail.com>
2019-05-31 18:25:28 -0400

    [WIP] Creating Tutorial

---
+++ MyFirstGame/Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    private int popUpIndex;
+
+    void Update() {
+        for (int i = 0; i < popUps.length; i++){
+        if(i == popUpIndex) {
+                popUps[popUpIndex].SetActive(true);
+            }else {
+                popUps[popUpIndex].SetActive(false);
+            }
+        }
+
+        if(popUpIndex == 0){
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                popUpIndexIndex++;
+            }
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow) || (Input.GetKeyDown(KeyCode.RightArrrow)) {
+         popUpIndexIndex++;
+
+        }
+    }
+
+}
